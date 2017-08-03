38d2fb3cd585e964c82d68012b2e239dc58d6e47
agustinchavez <chavez13@illinois.edu>
2017-08-03 16:27:05 -0500

    * NETReact.csproj: * packages.config: * elJefe.js: * bootstrap.js: * react.js: * Index.cshtml: * jquery-3.1.1.js: * elJefe.jsx: * bootstrap.min.js: * bootstrap.min.css: * bootstrap.css.map: * react-dom.js: * react.min.js: * jquery-3.1.1.min.js: * bootstrap-theme.css: * jquery-3.1.1.min.map: * jquery-3.1.1.slim.js: * jquery-3.1.1-vsdoc.js: * bootstrap.min.css.map: * react-dom.min.js: * bootstrap-theme.css.map: * bootstrap-theme.min.css: * jquery-3.1.1.slim.min.js: * react-dom-server.js: * jquery-3.1.1.slim.min.map: * react-with-addons.js: * bootstrap-theme.min.css.map: * react-dom-server.min.js: * react-with-addons.min.js: * glyphicons-halflings-regular.svg: * glyphicons-halflings-regular.eot: * glyphicons-halflings-regular.ttf: * glyphicons-halflings-regular.woff: * glyphicons-halflings-regular.woff2:
    * bootstrap.css: added reactjs package, changed tags in Index

---
+++ Scripts/react/elJefe.jsx
@@ -0,0 +1,13 @@
+﻿var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! 나는 코멘트 박스 다.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
